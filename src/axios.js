0de68279e70667bdd822fc8f562be4edf2575925
MAYANK816 <mayank0218.cse19@chitkara.edu.in>
2021-10-08 23:21:14 +0530

    tinder-frontend

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 const instance=axios.create({
-    baseURL:'https://tinder-backend-blond.vercel.app/'
+    baseURL:'https://tinderclone-project-backend.herokuapp.com/'
 })
 export default instance;
