477d919add06469b6687994daad9000669136ad4
MAYANK Khatri <63389169+MAYANK816@users.noreply.github.com>
2022-05-10 11:54:02 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 const instance=axios.create({
-    baseURL:'https://tinderclone-project-backend.herokuapp.com'
+    baseURL:'https://tinder-clone-2022.herokuapp.com'
 })
 export default instance;
